0833f4e8c9f84c42422ce98ec439e8d38749c1cc
Minmin Tian <tianmin0715@gmail.com>
2015-09-16 15:52:12 -0400

    login.html login.css controller/login.js

--- dashboard_app/app/scripts/controllers/login.js
+++ dashboard_app/app/scripts/controllers/login.js
@@ -1,1 +1,33 @@
-var app = angular.module("dashboard", []);
+var app = angular.module("dbApp");
+
+app.controller("login", function($scope, $http, $state) {
+    $scope.key = function() {
+        //alert('test');  
+        $scope.one = {
+            'opacity': '1'
+        }
+    };
+
+    $scope.login = function() {
+
+        var promise = $http.post('/api/login', {
+            userName: $scope.userName,
+            password: $scope.password
+        });
+        promise.then(function(response) {
+            if (response.data.authentication === "success") {
+                // Log the user in
+                //console.log("login");
+
+                $state.go("root");
+
+            } else {
+                // Not log the user in
+                console.log("error");
+            }
+        }, function() {
+            console.log("SORRY");
+        });
+    }
+
+});
