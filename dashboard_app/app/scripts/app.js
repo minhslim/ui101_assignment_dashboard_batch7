5a53eceb17ccfd217d17e7581f51b786c3409ebd
season <siwen.lu@hotmail.com>
2015-09-17 11:27:10 -0400

    no message

--- dashboard_app/app/scripts/app.js
+++ dashboard_app/app/scripts/app.js
@@ -13,12 +13,32 @@ angular.module('dbApp',[
 	 	.state('login',{
 			url:'/login',
 			templateUrl:'templates/login.html',
-			controller:'loginController'
+			controller:'login'
 		})
 		.state('root',{
-			url:'/root',
+			//url:'/root',
 			templateUrl:'templates/root.html',
 			controller:'rootController'
+		})
+		.state('root.overview',{
+			url:'/root/overview',
+			templateUrl:'templates/overview.html',
+			controller:'overview'
+		})
+		.state('root.work',{
+			url:'/root/work',
+			templateUrl:'templates/work.html',
+			controller:'work'
+		})
+		.state('root.contact',{
+			url:'/root/contact',
+			templateUrl:'templates/contact.html',
+			controller:'contact'
+		})
+		.state('root.producer',{
+			url:'/root/producer',
+			templateUrl:'templates/producer.html',
+			controller:'producer'
 		});
 
 });
