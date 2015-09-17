42e23cc66aee4c5648ef50ee46e01e6a9c5cd633
Minmin Tian <tianmin0715@gmail.com>
2015-09-17 14:12:00 -0400

    no message

--- dashboard_app/app/scripts/services/authenticate.js
+++ dashboard_app/app/scripts/services/authenticate.js
@@ -1,47 +1,22 @@
-
-// app.factory('authenticate', function($http, $cookieStore) {
-//     var user = $cookieStore.get('user');
-//     var psw = $cookieStore.get('psw');
-//     var promise = $http.post('/api/login', {
-//         userName: user,
-//         password: psw
-//     });
-
-
-
-
-
-
-// });
 var app = angular.module("dbApp");
-app.factory('authenticate', function($http,$cookieStore) {
-	var res;
-	var user = $cookieStore.get('user');
+app.factory('authenticate', function($http, $cookieStore) {
+    var res;
+    var user = $cookieStore.get('user');
     var psw = $cookieStore.get('psw');
     var promise = $http.post('/api/login', {
-             userName: user,
-             password: psw
-         });
-        promise.then(function(response) {
-        	res = response.data.authentication;
-            // if (response.data.authentication === "success") {
-            //     // Log the user in
-            //     //console.log("login");
+        userName: user,
+        password: psw
+    });
+    promise.then(function(response) {
+        res = response.data.authentication;
 
-            //     $state.go("root");
-
-            // } else {
-            //     // Not log the user in
-            //     console.log("error");
-            // }
-        }, function() {
-            console.log("SORRY");
-        });
-	return {
-		getUser: function() {
-		return res;
-			
-
-		}
-	};
+    }, function() {
+        console.log("SORRY");
+    });
+    return {
+        getUser: function() {
+            return res;
+        }
+    };
 });
+
