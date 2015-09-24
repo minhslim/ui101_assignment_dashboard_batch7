a04cd5be1b6e21a3d611eaee4e1cb3e7ca494b90
fei <yufei4594@gmail.com>
2015-09-24 15:15:04 -0400

    test

--- dashboard_app/test/spec/controllers/login.js
+++ dashboard_app/test/spec/controllers/login.js
@@ -1,16 +1,16 @@
 'use strict';
 
 describe('Controller: login', function(){
-	// beforeEach(module('dbApp'));
-	// var login,
-	// 	scope;
-	// beforeEach(inject(function($Controller,$rootScope){
-	// 	scope=$rootScope.$new();
-	// 	login=$controller('login',{
-	// 		$scope:scope
-	// 	});
-	// }));
-	// it('should have an empty default error message in the scope', function(){
-	// 	expect(scope.errorMsg.length).toBe(0);
-	// });
+	beforeEach(module('dbApp'));
+	var login,
+		scope;
+	beforeEach(inject(function($Controller,$rootScope){
+		scope=$rootScope.$new();
+		login=$controller('login',{
+			$scope:scope
+		});
+	}));
+	it('should have an empty default error message in the scope', function(){
+		expect(scope.errorMsg.length).toBe(0);
+	});
 });
